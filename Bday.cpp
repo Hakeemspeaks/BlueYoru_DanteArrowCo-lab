b772a048d051c297da16a5336d389e1bba447831
Hakeem D. Abdulmalik <hakeemspeaks@gmail.com>
2016-11-04 00:30:44 -0400

    Bday.cpp
    Input for birthday and validation for birthday. Created by Dante.

---
+++ Bday.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomamip>
+
+using namespace std;
+
+int Bday()
+{
+  int FullBday, Bmonth;
+  
+  cout << endl << "Please enter the month your birthday is on: "; 
+  cin.ignore();
+  cin >> Bmonth;
+  cout << endl << endl;
+  
+  cout << endl << "Please enter the day your birthday is on: "; 
+  cin.ignore();
+  cin >> Bmonth;
+  cout << endl << endl;
+  
+  cout << endl << "Please enter the year your birthday is on: "; 
+  cin.ignore();
+  cin >> Bmonth;
+  cout << endl << endl;
+  
+  
+
+  return FullBday;
+}
